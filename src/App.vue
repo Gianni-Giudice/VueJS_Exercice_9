72d15747deee0bea02ad138727199224222399a7
Gianni-Giudice <62549276+Gianni-Giudice@users.noreply.github.com>
2020-03-23 15:37:34 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
   <div id="app">
     <Navbar/>
-    <Home/>
-    <Form/>
-    <About/>
+    <router-view/>
   </div>
 </template>
 
 <script>
+
 import Navbar from './components/Navbar.vue';
-import Home from './components/Home.vue';
-import Form from './components/Form.vue';
-import About from './components/About.vue';
 
 export default {
   name: 'App',
   components: {
-    Navbar, 
-    Home, 
-    Form,
-    About
+    Navbar
   }
 }
 </script>
